aa66936860dff917a81e1d348af405f5c7d65ea3
nmcxx <maccervn@gmail.com>
2021-01-27 10:01:37 +0700

    completed commit

--- TestSpringBoot/src/main/java/com/example/testspringboot/User.java
+++ TestSpringBoot/src/main/java/com/example/testspringboot/User.java
@@ -18,18 +18,9 @@ public class User {
 		return username;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
 	public String getPassword() {
 		return password;
 	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
 	
 	
 	
